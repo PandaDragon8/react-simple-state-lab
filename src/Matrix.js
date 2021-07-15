c7a179da2a4a15953edcab24e9574528831758dd
Min <mtan121@aucklanduni.ac.nz>
2021-07-15 22:37:01 +0800

    Done with assignment

--- src/Matrix.js
+++ src/Matrix.js
@@ -8,6 +8,7 @@ export default class Matrix extends Component {
   }
   
   genMatrix = () => {
+    console.log(genMatrix)
     return this.props.values.map(rowVals => <div className="row">{this.genRow(rowVals)}</div>)
   }
   
